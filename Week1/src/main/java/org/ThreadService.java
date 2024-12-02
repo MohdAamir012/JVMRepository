bc1e0607d3a7687e807391504777d2312f89e8b1
Mohd Aamir <mohd.aamir@tothenew.com>
2024-12-02 23:06:49 +0530

    First commit

---
+++ Week1/src/main/java/org/ThreadService.java
@@ -0,0 +1,11 @@
+package org;
+
+public class ThreadService extends Thread{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
